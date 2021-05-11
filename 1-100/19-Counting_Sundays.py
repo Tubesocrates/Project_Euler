e4e141c6a3d7ddff8a676869f613f97e1c71370c
Tubesocrates <josh.johnson.w@gmail.com>
2021-05-11 17:55:37 -0400

    staged

--- 1-100/19-Counting_Sundays.py
+++ 1-100/19-Counting_Sundays.py
@@ -15,38 +15,69 @@ How many Sundays fell on the first of the month during the twentieth century (1 
 """
 import math, statistics, numpy as np
 
-Months = {Jan: 31
-          Feb: 28
-          Mar: 31
-          Apr: 30
-          May: 31
-          Jun: 30
-          Jul: 31
-          Aug: 31
-          Sep: 30
-          Oct: 31
-          Nov: 30
-          Dec: 31
-          F3b: 29
-         }
+Months = {"Jan": 31,
+          "Feb": 28,
+          "Mar": 31,
+          "Apr": 30,
+          "May": 31,
+          "Jun": 30,
+          "Jul": 31,
+          "Aug": 31,
+          "Sep": 30,
+          "Oct": 31,
+          "Nov": 30,
+          "Dec": 31,
+        }
+
+month_keys = list(Months.keys())
+
               
-Days = [Mon, Tue, Wed, Thu, Fri, Sat, Sun]
+Days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 def day_runner(desired_date):
-    cali = [[Jan, 1, 1900], [Monday]]
-    a = cali.copy()
+    a = [["Jan", 1, 1900], ["Mon"]]
     
-    while a[0][3] < desired_date[0][3]:
-        
+    month = a[0][0]
+    DOM = a[0][1]
+    year = a[0][2]
 
+    DOW = a[1]
 
-
-
-def Sunday_Counter(Start_Date, End_Date): # day Mon year
-    Start_Date = Start_Date.split(" ")
-    End_Date = End_Date.split(" ")
-    while int(Start_Date[2]) <= int(End_Date[2]):
-        Sun_Counter = 0
-        Start_Date[0]
-        
+    year_0 = a[0][2]
+    Sun_count = 0
+    month_count = 0
+    Sun_First_count = 0
+    dow_temp = False
     
+    while year < desired_date[2]:
+        if year % 4 == 0:
+            Months["Feb"] = 29
+        elif year % 1000 and year % 400:
+            Months["Feb"] = 29
+        else:
+            Months["Feb"] = 28
+        index = 0
+        if year == year_0:
+            index = month_keys.index(month)
+        while index < len(month_keys):
+            month_len = Months[month_keys[index]]
+            while DOM < month_len + 1:
+                if not dow_temp:
+                    DOW = Days[(DOM % 7) - 1]
+                else:
+                    DOW_0 = Days.index(dow_temp)
+                    DOW = Days[(((DOW_0) % 7) - 1)]
+                if (DOW == "Sun"):
+                    Sun_count += 1
+                    if DOM < 2:
+                        Sun_First_count += 1
+                DOM += 1
+                dow_temp = DOW
+            index += 1
+            DOM = 1
+            month_count += 1
+        year += 1
+        # print(year, Sun_count, Sun_First_count, month_count)
+    return Sun_First_count
+
+print(day_runner([31, "Dec", 2000]))
